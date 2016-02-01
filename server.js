ca74164491d4404d67932e2f7daa9e36db681129
paycoguy <aalonzo@gmail.com>
2016-02-01 18:51:04 +0000

    added chart

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ var routes = require('./app/routes/index.js');
 var app = express();
 require('dotenv').load();
 
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use('/css', express.static(process.cwd() + '/public/css'));
+app.use('/js', express.static(process.cwd() + '/public/js'));
 	app.route('/')
 		.get(function (req, res) {
 			res.sendFile(process.cwd() + '/public/index.html');
 		});
 		
-//routes(app);
 
 var port = process.env.PORT || 8080;
 app.listen(port,  function () {
